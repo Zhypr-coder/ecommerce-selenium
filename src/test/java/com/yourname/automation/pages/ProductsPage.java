e85890046ae7dd7a63f4eea39a372de4983e0be0
ishwar <irbttase2002@gmail.com>
2026-08-01 23:22:58 +0530

    Fix cart page navigation timing issue

--- src/test/java/com/yourname/automation/pages/ProductsPage.java
+++ src/test/java/com/yourname/automation/pages/ProductsPage.java
@@ -3,6 +3,7 @@ package com.yourname.automation.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class ProductsPage extends BasePage {
 
@@ -29,6 +30,7 @@ public class ProductsPage extends BasePage {
 
     public CartPage goToCart() {
         click(cartIcon);
+        wait.until(ExpectedConditions.urlContains("cart"));
         return new CartPage();
     }
 }
